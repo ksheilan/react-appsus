2a5bc8d81321a4c8299106350c19fbb31c5b347c
ofek ashkenazi <ofeka18@gmail.com>
2022-12-31 16:57:35 +0200

    add some css to mail preview. add body msg to mailDataList.

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -8,7 +8,6 @@ const loggedinUser = {
     fullname: 'Mahatma Appsus'
 }
 
-
 _createEmails()
 
 export const MailServices = {
@@ -32,7 +31,7 @@ function _createEmails() {
                 isRead: true,
                 sentAt: 1551133930594,
                 to: loggedinUser.email,
-                from: 'Harry potter',
+                from: 'Harry potter@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
@@ -40,18 +39,16 @@ function _createEmails() {
             },
             {
                 id: 'e102',
-                subject: 'Your paypal account is log off',
-                body: 'Would love to catch up sometimes',
+                subject: 'Coffee-Making Software',
+                body: 'Attention coffee lovers! Theres a revolution coming your way Coffee has been a popular drink since who knows when? But have you ever thought about how much time people spend making their morning cuppa? The process is laborious and cumbersome. It takes so much work for something so simple. But not anymore!AI-powered coffee machines that grind beans, measure coffee grounds, and weigh them for optimal brewing have come to save the day. Allowing you to just take a sip in the morning without any hassle. So easy!Be a part of this tech revolution today by buying a product from a tech company who cares about your daily routine. See exclusive audience insights here',
                 isRead: true,
                 sentAt: 18761930594,
                 to: loggedinUser.email,
-                from: 'Harry potter',
+                from: 'Coffee-Making@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e103',
@@ -65,8 +62,6 @@ function _createEmails() {
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e104',
@@ -75,12 +70,11 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 265553430894,
                 to: 'momo@momo.com',
-                from: 'Harry potter',
+                from: 'Biance@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
             },
             {
                 id: 'e105',
@@ -89,13 +83,11 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 26281338894,
                 to: 'momo@momo.com',
-                from: 'Harry potter',
+                from: 'GitHub@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e106',
@@ -104,28 +96,24 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 26245133550894,
                 to: 'momo@momo.com',
-                from: 'Harry potter',
+                from: 'Google platform@.com',
                 isDelete: true,
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e107',
-                subject: 'לוח דרושים',
-                body: 'Would love to catch up sometimes',
+                subject: 'bamba',
+                body: 'Kids love bamba! Bamba is a tasty and nutritious snack that kids of all ages enjoy, Bamba is a delicious peanut butter-flavored corn puff that kids of all ages love. Made with 100% natural ingredients, Bamba offers an irresistible combination of crunchy and smooth textures. Feed the little ones who refuse to eat their vegetables by giving them something they, Its bamba! Bamba is an Israeli snack that consists of a peanut butter-flavored puffed corn cereal',
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
-                from: 'Harry potter',
+                from: 'Bamba@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e108',
@@ -139,13 +127,11 @@ function _createEmails() {
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e109',
-                subject: 'Google Maps Platform',
-                body: 'Would love to catch up sometimes',
+                subject: 'Shoko',
+                body: 'Thirst-quenching water for the people. shoko is a refreshing, calorie-free drink that wont leave you feeling thirsty or weighed down. Try it today! Drink with shoko. A refreshing drink that is sugar-free and full of flavor. Drink up! Life is better when youre hydrated. Keep yourself and your family healthy with shoko drink, the healthier alternative to sodas and other sugary drinks.',
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
@@ -154,13 +140,11 @@ function _createEmails() {
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e110',
-                subject: 'לוח דרושים',
-                body: 'Would love to catch up sometimes',
+                subject: 'Computers',
+                body: 'What do you need a computer for? Were the specialists when it comes to computers. Give us a call today! When it comes to computers, you cant get much better than a Lenovo. Built with the highest quality components and backed by world-class customer service, Lenovo is the ultimate choice for desktops and laptops. Order today! For the best in quality and service, you need a computer that was built with care. Our computers are custom-built to order by technicians who understand your needs. Call us today for more information!',
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
@@ -169,13 +153,11 @@ function _createEmails() {
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e111',
-                subject: 'Google Maps Platform',
-                body: 'Would love to catch up sometimes',
+                subject: 'House sale',
+                body: 'Buy a house. Houses for sale. Call today!Stop looking at houses and buy one. Buying a house is the American dream—but it doesnt have to be complicated or stressful. Let us help you find a home that fits both your needs and budget. Call for more information today!',
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
@@ -184,13 +166,11 @@ function _createEmails() {
                 isStarred: true,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e112',
-                subject: 'לוח דרושים',
-                body: 'Would love to catch up sometimes',
+                subject: 'hospitel document',
+                body: 'You dont have to worry about getting a sunburn with sunscreen! Our new lotion is SPF 50 and protects against both UVA and UVB rays. Click here to order today. Dont get a sunburn this year. Reapply sunscreen every 2 hours and reapply after you been in the water or sweating for an extended period of time.',
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
@@ -199,7 +179,6 @@ function _createEmails() {
                 isStarred: true,
                 isDraft: false,
                 furterTreatment: false,
-
             },
             {
                 id: 'e113',
@@ -208,12 +187,11 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
-                from: 'DRAFTEST',
+                from: 'DRAFTEST@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: true,
                 furterTreatment: false,
-
             },
             {
                 id: 'e114',
@@ -222,7 +200,7 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 265553430894,
                 to: 'momo@momo.com',
-                from: 'Harry potter',
+                from: 'Biance@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
@@ -230,18 +208,16 @@ function _createEmails() {
             },
             {
                 id: 'e115',
-                subject: 'לוח דרושים',
-                body: 'Would love to catch up sometimes',
+                subject: 'hospitel document',
+                body: 'You dont have to worry about getting a sunburn with sunscreen! Our new lotion is SPF 50 and protects against both UVA and UVB rays. Click here to order today. Dont get a sunburn this year. Reapply sunscreen every 2 hours and reapply after you been in the water or sweating for an extended period of time.',
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
-                from: loggedinUser.email,
+                from: 'kapln@.com',
                 isDelete: false,
-                isStarred: false,
+                isStarred: true,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e116',
@@ -255,22 +231,19 @@ function _createEmails() {
                 isStarred: true,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e117',
-                subject: 'לוח דרושים',
-                body: 'Would love to catch up sometimes',
+                subject: 'bamba',
+                body: 'Kids love bamba! Bamba is a tasty and nutritious snack that kids of all ages enjoy, Bamba is a delicious peanut butter-flavored corn puff that kids of all ages love. Made with 100% natural ingredients, Bamba offers an irresistible combination of crunchy and smooth textures. Feed the little ones who refuse to eat their vegetables by giving them something they, Its bamba! Bamba is an Israeli snack that consists of a peanut butter-flavored puffed corn cereal',
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
-                from: loggedinUser.email,
+                from: 'Bamba@.com',
                 isDelete: false,
                 isStarred: true,
                 isDraft: false,
                 furterTreatment: false,
-
             },
             {
                 id: 'e118',
@@ -279,12 +252,11 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
-                from: 'DRAFTEST',
+                from: 'DRAFTEST@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: true,
                 furterTreatment: false,
-
             },
             {
                 id: 'e119',
@@ -293,7 +265,7 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 265553430894,
                 to: 'momo@momo.com',
-                from: 'Harry potter',
+                from: 'Harry potter@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
@@ -306,13 +278,11 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 26245133550894,
                 to: 'momo@momo.com',
-                from: 'Harry potter',
+                from: 'Harry potter@.com',
                 isDelete: true,
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e120',
@@ -321,13 +291,11 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
-                from: 'Harry potter',
+                from: 'Mario8@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e121',
@@ -336,13 +304,11 @@ function _createEmails() {
                 isRead: false,
                 sentAt: 1551133930594,
                 to: 'momo@momo.com',
-                from: loggedinUser.email,
+                from: 'GitHub26@.com',
                 isDelete: false,
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
             {
                 id: 'e122',
@@ -356,8 +322,6 @@ function _createEmails() {
                 isStarred: false,
                 isDraft: false,
                 furterTreatment: false,
-
-
             },
         ]
         storageService.saveToStorage(EMAIL_STOARGE_KEY, emails)
